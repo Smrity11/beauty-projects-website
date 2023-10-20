35363f8a8647d91c058eb42b8d92962f8529ba23
Smrity11 <kanisasia376@gmail.com>
2023-10-20 10:38:36 -0700

    add advertisement slider

--- src/components/AllBrand/Dior.jsx
+++ src/components/AllBrand/Dior.jsx
@@ -10,7 +10,38 @@ const Dior = () => {
    const filteredProducts = lorealLoader.filter(product => product.brand === "Dior");
 
    return (
-     <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 justify-center items-center">
+  <div className="space-y-6 p-8 lg:px-3">
+<h1 className="text-4xl font-bold "> All of todays best <span className=" font-extrabold text-amber-600">selling</span> products</h1>
+<p className="text-2xl font-semibold">clients testimonials</p>
+<p className="text-slate-500 leading-7">Fear is huge issue for me .This technique .This brand has is really smart way to literally set those fears aside ... That to me ,is  unleasing the product whithin .Through brand products work, I found my strength</p>
+  </div>
+  <div className="col-span-2">
+  <div className="carousel w-full h-[450px] ">
+  <div id="item1" className="carousel-item w-full">
+    <img src="https://assets.gadgets360cdn.com/pricee/assets/category/202006/brand-loreal-1200x628_1591871140.jpg" className="w-full" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="https://www.sheknows.com/wp-content/uploads/2023/05/Best-LOreal-Products-for-Mature-Skin-TKTK-e1683129257433.jpg?w=1440" className="w-full" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src="https://www.lorealparisusa.com/-/media/project/loreal/brand-sites/oap/americas/us/beauty-magazine/articles/best-skin-care-products-for-dry-skin/loreal-paris-article-our-best-skin-care-products-for-dry-skin-d.jpg" className="w-full" />
+  </div> 
+ 
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+ 
+</div>
+  </div>
+</div>
+
+
+
+<div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
        {filteredProducts.map(product => (
          <div key={product._id} className="card bg-base-100 shadow-xl">
            <figure className="px-2 pt-10">
@@ -38,6 +69,7 @@ const Dior = () => {
          </div>
        ))}
      </div>
+    </div>
    );
 };
 
--- src/components/BrandDetails.jsx
+++ src/components/BrandDetails.jsx
@@ -1,21 +1,37 @@
-// import { useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 
 const BrandDetails = () => {
-    // const brandDataLoad = useLoaderData()
+    const brandDataLoad = useLoaderData()
+    const {singleData, image, name, price, rating, description} = brandDataLoad
+    console.log(singleData)
     return (
-        <div>
-            <div>
-                <img src=""  alt="" />
-                <div>
-                    <p>Name:  </p>
-                    <p>type:</p>
-                    <p>Brand Name:</p>
-                    <p>Price:</p>
-                    <p>Ratings:</p>
-                </div>
-            </div>
+       <>
+       <h2 className="text-3xl font-bold text-center my-12 text-gray-700">Product Details</h2>
+         <div className="w-full my-12 lg:w-[800px] mx-auto rounded overflow-hidden shadow-lg">
+        <img 
+        
+        src={image} alt={name} className="w-full h-[400px]" />
+  
+        <div className="p-4">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base">{description}</p>
         </div>
+  
+    <div className="flex justify-between items-center">
+    <div className="px-6 py-4">
+          <span className="text-gray-700 text-base">Price: {price}</span>
+          <br />
+          <span className="text-gray-700 text-base">Rating: {rating}</span>
+        </div>
+        <div className="me-8 btn btn-success normal-case">
+            <button>Add to Card</button>
+        </div>
+    </div>
+        </div>
+      </div>
+       </>
     );
 };
 
